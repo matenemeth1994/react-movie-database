cf601f8200084a088e0596859cfc90c2bb745194
matenemeth1994 <mate.nemeth@nology.io>
2020-04-13 18:36:18 +0100

    changed reducer to states

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import Movie from "./Components/Movie";
@@ -6,83 +6,44 @@ import Search from "./Components/Search";
 
 const MOVIE_API_URL = "https://www.omdbapi.com/?s=spider&apikey=1e50f614";
 
-const initialState = {
-  loading: true,
-  movies: [],
-  errorMessage: null,
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "SEARCH_MOVIES_REQUEST":
-      return {
-        ...state,
-        loading: true,
-        errorMessage: null,
-      };
-    case "SEARCH_MOVIES_SUCCESS":
-      return {
-        ...state,
-        loading: false,
-        movies: action.payload,
-      };
-    case "SEARCH_MOVIES_FAILURE":
-      return {
-        ...state,
-        loading: false,
-        errorMessage: action.error,
-      };
-    default:
-      return state;
-  }
-};
-
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     fetch(MOVIE_API_URL)
       .then((response) => response.json())
       .then((jsonResponse) => {
-        dispatch({
-          type: "SEARCH_MOVIES_SUCCESS",
-          payload: jsonResponse.Search,
-        });
+        setMovies(jsonResponse.Search);
+        setLoading(false);
       });
   }, []);
 
   const search = (searchValue) => {
-    dispatch({
-      type: "SEARCH_MOVIES_REQUEST",
-    });
+    setLoading(true);
+    setErrorMessage(null);
 
     fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=1e50f614`)
       .then((response) => response.json())
       .then((jsonResponse) => {
         if (jsonResponse.Response === "True") {
-          dispatch({
-            type: "SEARCH_MOVIES_SUCCESS",
-            payload: jsonResponse.Search,
-          });
+          setMovies(jsonResponse.Search);
+          setLoading(false);
         } else {
-          dispatch({
-            type: "SEARCH_MOVIES_FAILURE",
-            error: jsonResponse.Error,
-          });
+          setErrorMessage(jsonResponse.Error);
+          setLoading(false);
         }
       });
   };
 
-  const { movies, errorMessage, loading } = state;
-
   return (
     <div className="App">
-      <Header text="React Movie Database Tech Test" />
+      <Header text="React Movie Database" />
       <Search search={search} />
-      <p className="App-intro">Sharing a few of our favourite movies</p>
       <div className="movies">
         {loading && !errorMessage ? (
-          <span>loading... </span>
+          <span>loading...</span>
         ) : errorMessage ? (
           <div className="errorMessage">{errorMessage}</div>
         ) : (
--- src/Components/Movie/Movie.jsx
+++ src/Components/Movie/Movie.jsx
@@ -22,3 +22,4 @@ const Movie = ({ movie }) => {
 };
 
 export default Movie;
+
